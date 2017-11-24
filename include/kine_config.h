79b039f93aee8663ac80d213589d85888391c7a4
EikiObara <tas30947@gmail.com>
2017-11-24 16:20:45 +0900

    重心算出プログラムが完成.

--- include/kine_config.h
+++ include/kine_config.h
@@ -9,52 +9,73 @@
 namespace Trl {
 //-----Over all-----
 const int kMaxJoint = 7;
+
 const int kMaxLink = 8;
 
 typedef Eigen::MatrixXd JointT;
+
 typedef Eigen::MatrixXd JointVelT;
-typedef Eigen::MatrixXd TolqueT;
+
+typedef Eigen::Vector3d TolqueT;
+
 typedef Eigen::Matrix<double,kMaxLink,1> LinkT;
 
-typedef Eigen::Matrix4d RotMatT;
+typedef Eigen::Matrix4d RotMatT;	//本当は同時変換行列だけど便宜的に回転行列としています
 
-typedef Eigen::Matrix<double,3,1> CoGT;
+typedef Eigen::Vector3d CoGT;
+
 
 //-----Spline-----
 const int ROUTE_POINTS = 3;
 
-const int ROUTE_LINK = ROUTE_POINTS - 1;
+const int ROUTE_LINK = ROUTE_POINTS - 1;	// link number is defined by nodes. (there is one link between two nodes, so maxLinkVal = maxNodeVal - 1)
 
-const double VIA_LENGTH = 50;
+const double VIA_LENGTH = 50;		//it uses for CalcViaPos in kine_trajectory.h ... however, it will need not to use. 
 
+// constant numbers 
 namespace CHRConfig{
-//-----arm parameter-----
+//-----arm length parameter-----
 
 //const double kShoulder	= 164.0;	//肩原点のとき使わない
 const double kUpper	= 322.0;
 const double kFore	= 257.0;
 const double kHand	= 154.0;
 
-const double kALen[kMaxLink]= {0.0,0.0,kUpper,0.0,kFore,0.0,0.0,kHand};
-const double kDLen[kMaxLink]= {};
+const double kArmLen[kMaxLink]= {0.0,0.0,kUpper,0.0,kFore,0.0,0.0,kHand};
+const double kOffsetLen[kMaxLink]= {};
 const double kAlpha[kMaxLink]= {0.0,-M_PI/2,M_PI/2,-M_PI/2,M_PI/2,-M_PI/2,M_PI/2,0.0};
+
+//-----CoG length from center of joint-----
+const double CoG_UPPER		= 33.89;	//[mm]
+const double CoG_FORWARD	= 142.23;	//[mm]
+const double CoG_HAND		= 22.3;	//[mm]
+const double kCoGLen[3] = {CoG_UPPER,CoG_FORWARD,CoG_HAND};
+
+//-----  WEIGHT param. -----
+const double WEIGHT_UPPER	= 1.00518;
+const double WEIGHT_FORWARD	= 0.56333;
+const double WEIGHT_HAND	= 0.268;
+const double kWeight[3] = {WEIGHT_UPPER,WEIGHT_FORWARD,WEIGHT_HAND};
+
+const double gravityAcceleration = 9806.65;	//m/s^2
+const double kGravAccel[3] = {0,-gravityAcceleration, 0};
 
 }	//namespace CHRConfig
 
-const LinkT kALength(CHRConfig::kALen);
-const LinkT kDLength(CHRConfig::kDLen);
+//-------------------------------------------------
+//----- constant numbers converted eigen type -----
+//-------------------------------------------------
+const LinkT kArmLength(CHRConfig::kArmLen);
+
+const LinkT kOffsetLength(CHRConfig::kOffsetLen);
+
 const LinkT kAlphaRad(CHRConfig::kAlpha);
 
-const double CoG_UPPER		= 33.89;
-const double CoG_FORWARD	= 142.23;
-const double CoG_HAND		= 22.3;
+const Eigen::Vector3d kCoGLength(CHRConfig::kCoGLen);
 
-const double kCoG[3] = {CoG_UPPER,CoG_FORWARD,CoG_HAND};
+const Eigen::Vector3d kLinkWeight(CHRConfig::kWeight);
 
-const double WEIGHT_UPPER	= 1005.18;
-const double WEIGHT_FORWARD	= 563.33;
-const double WEIGHT_HAND	= 0.268;
+const Eigen::Vector3d kGravityAccel(CHRConfig::kGravAccel);
 
-const double kWeight[3] = {WEIGHT_UPPER,WEIGHT_FORWARD,WEIGHT_HAND};
 }	//namespace Trl
 #endif // __MY_CONFIG_H__
---
+++ include/kine_coords.h
@@ -0,0 +1,59 @@
+
+#ifndef __KINE_COORDS_H__
+#define __KINE_COORDS_H__
+
+#include "kine_htm.h"
+#include <vector>
+#include "kine_config.h"
+
+namespace Trl{
+
+class Coords : private HTM{
+public:
+	Coords(int maxJoint);
+	~Coords();
+
+	void InitCoords(Eigen::MatrixXd &curJointRad);
+
+	void GetFinger(Eigen::Vector3d &coords);
+	void GetWrist (Eigen::Vector3d &coords);
+	void GetElbow (Eigen::Vector3d &coords);
+};
+
+Coords::Coords(int maxJoint):HTM(maxJoint){
+	SetArmLength(kArmLength);
+	SetOffsetParam(kOffsetLength);
+	SetAlphaParam(kAlphaRad);
+}
+
+Coords::~Coords(){}
+
+void Coords::InitCoords(Eigen::MatrixXd &curJointRad){
+	CalcHTM(curJointRad);
+	GetHTMAll();
+}
+
+//magic number description(this params can use at 7dof arm only)
+// 7 : arm's end effector position
+// 6 : arm's wrist position
+// 4 : arm's elbow position
+
+void Coords::GetFinger(Eigen::Vector3d &coords){
+	coords.resize(3);
+	for(int i = 0; i < 3; ++i)coords(i) = htm[7](i,3);
+}
+
+void Coords::GetWrist(Eigen::Vector3d &coords){
+	coords.resize(3);
+	for(int i = 0; i < 3; ++i)coords(i) = htm[6](i,3);
+}
+
+void Coords::GetElbow(Eigen::Vector3d &coords){
+	coords.resize(3);
+	for(int i = 0; i < 3; ++i)coords(i) = htm[3](i,3);
+}
+
+
+}	//namespace Trl
+
+#endif //__KINE_COORDS_H__
